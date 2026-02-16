cd322215b19e4f0895f6dbad9983d6e0952694dd
Murat Ertik <dmuratertik1@gmail.com>
2026-02-17 00:50:34 +0300

    first commit

---
+++ src/main/java/org/example/contributetolearning/ContributeToLearningApplication.java
@@ -0,0 +1,13 @@
+package org.example.contributetolearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributeToLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContributeToLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/contributetolearning/ContributeToLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.contributetolearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContributeToLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
