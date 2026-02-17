ce7e1165f0e5f0152a358a6ae4116a09052a9f4f
Murat Ertik <dmuratertik1@gmail.com>
2026-02-17 19:40:13 +0300

    rest api config added for manage controllers.

---
+++ src/main/java/org/example/contributetolearning/configs/RestApis.java
@@ -0,0 +1,21 @@
+package org.example.contributetolearning.configs;
+
+public class RestApis {
+
+    //Essential Part Start
+
+    public static final String DEVELOPER = "/dev";
+    public static final String RELEASE = "/prod";
+    public static final String VERSIONS = "/v1";
+
+    //Essential Part End
+    //Repository Part Start
+
+    public static final String REPOSITORIES = DEVELOPER+VERSIONS+"/repositories";
+    public static final String CREATE = "/create";
+    public static final String GET_ALL = "/get-all";
+
+
+    //Repository Part End
+    //
+}
