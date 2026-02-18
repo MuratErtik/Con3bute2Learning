55ec5c412fe986e75a33f7417026694ed2cdcd0a
Murat Ertik <dmuratertik1@gmail.com>
2026-02-18 23:13:45 +0300

    command line added.

--- src/test/java/org/example/contributetolearning/ContributeToLearningApplicationTests.java
+++ src/test/java/org/example/contributetolearning/ContributeToLearningApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+//that annotation will help to make ApplicationContext(beans,configurations,db connections) up
 class ContributeToLearningApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//if context loads successfully, that test will be passed otherwise application down even not getting started
 	}
 
 }
