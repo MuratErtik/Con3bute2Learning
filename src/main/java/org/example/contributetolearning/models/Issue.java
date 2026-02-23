fc0b9f6ede394415618dfc4cdd6143c528c1f114
Murat Ertik <dmuratertik1@gmail.com>
2026-02-23 03:31:40 +0300

    issue entity created.

---
+++ src/main/java/org/example/contributetolearning/models/Issue.java
@@ -0,0 +1,29 @@
+package org.example.contributetolearning.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Issue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String body;
+
+}
