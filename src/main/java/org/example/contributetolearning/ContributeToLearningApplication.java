12ee21a55386c464fac6313d9b920f3b83333225
Murat Ertik <dmuratertik1@gmail.com>
2026-02-24 01:43:04 +0300

    enable to schedule.

--- src/main/java/org/example/contributetolearning/ContributeToLearningApplication.java
+++ src/main/java/org/example/contributetolearning/ContributeToLearningApplication.java
@@ -2,8 +2,10 @@ package org.example.contributetolearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ContributeToLearningApplication {
 
 	public static void main(String[] args) {
