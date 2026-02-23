49cccb131b4feef483aecf03e74b6f4f4b210b02
Murat Ertik <dmuratertik1@gmail.com>
2026-02-24 01:43:42 +0300

    query added to endpoint.

--- src/main/java/org/example/contributetolearning/services/GithubClientService.java
+++ src/main/java/org/example/contributetolearning/services/GithubClientService.java
@@ -10,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Service
 @RequiredArgsConstructor
 public class GithubClientService {
@@ -20,9 +23,9 @@ public class GithubClientService {
 
 
 
-    public GithubIssueResponse[] listIssues(String owner, String repository) {
+    public GithubIssueResponse[] listIssues(String owner, String repository, LocalDate since) {
 
-        String issuesUrl = String.format("%s/repos/%s/%s/issues",properties.getApiUrl(), owner, repository);
+        String issuesUrl = String.format("%s/repos/%s/%s/issues?since=%s",properties.getApiUrl(), owner, repository,since.toString());
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Token " + properties.getToken());
